306b3da639a65361fc6287df4e0bd3c4c11a6048
Paul <Neminem1203@users.noreply.github.com>
2021-01-26 14:22:38 -0500

    Addon template created

--- core/src/com/mygdx/adonis/AddOn.java
+++ core/src/com/mygdx/adonis/AddOn.java
@@ -3,14 +3,23 @@ package com.mygdx.adonis;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-public abstract class AddOn {
+
+
+public class AddOn {
     public Rectangle hitbox;
 
     // TODO position relative to ship?
     // e.g. add-on on left vs right side of ship?
     // we might decide to just generically increase size of ship hitbox, negating need for each
     // add-on to have its own
-    public abstract void update(float delta);
 
-    public abstract void draw(SpriteBatch spriteBatch);
+    public AddOn(int id, String name){
+        this.id = id;
+        this.name = name;
+        this.data = new AddOnData(id, name);
+    }
+
+//    public abstract void update(float delta);
+//
+//    public abstract void draw(SpriteBatch spriteBatch);
 }
